50704c497bddb9822aa13c7583a7c9f3f0147989
lifework <kobayashi@lifework.tech>
2023-01-01 23:58:13 +0900

    C3.1.3: App を Hello に変更

--- src/react-sample/src/index.tsx
+++ src/react-sample/src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './components/Hello';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Hello />
   </React.StrictMode>
 );
 
